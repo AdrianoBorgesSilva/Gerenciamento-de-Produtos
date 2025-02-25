841ffdfbf8c95a7348ba57324500231a86d01df4
Adriano Borges Silva <120127965+AdrianoBorgesSilva@users.noreply.github.com>
2025-02-25 17:14:32 -0300

    Uso de Lombok

--- src/main/java/com/example/product_manager/domain/Product.java
+++ src/main/java/com/example/product_manager/domain/Product.java
@@ -8,6 +8,13 @@ import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.example.product_manager.enums.ProductStatus;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
 @Document(collection = "product")
 public class Product implements Serializable{
 
@@ -23,10 +30,6 @@ public class Product implements Serializable{
     private String brand;
     private String category;
     private ProductStatus status;
-    
-    public Product() {
-
-    }
 
     public Product(String id, String name, String description, BigDecimal price, int quantity, ProductStatus status, String brand, String category) {
         this.id = id;
@@ -37,74 +40,6 @@ public class Product implements Serializable{
         this.brand = brand;
         this.category = category;
         this.status = status;
-    }
-
-    public static long getSerialversionuid() {
-        return serialVersionUID;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public BigDecimal getPrice() {
-        return price;
-    }
-
-    public void setPrice(BigDecimal price) {
-        this.price = price;
-    }
-
-    public int getQuantity() {
-        return quantity;
-    }
-
-    public void setQuantity(int quantity) {
-        this.quantity = quantity;
-    }
-
-    public ProductStatus getStatus() {
-        return status;
-    }
-
-    public void setStatus(ProductStatus status) {
-        this.status = status;
-    }
-
-    public String getCategory() {
-        return category;
-    }
-
-    public void setCategory(String category) {
-        this.category = category;
-    }
-
-    public String getBrand() {
-        return brand;
-    }
-
-    public void setBrand(String brand) {
-        this.brand = brand;
     }
 
     @Override
--- src/main/java/com/example/product_manager/dto/ProductDTO.java
+++ src/main/java/com/example/product_manager/dto/ProductDTO.java
@@ -9,7 +9,11 @@ import com.example.product_manager.enums.ProductStatus;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.PositiveOrZero;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@NoArgsConstructor
 public class ProductDTO implements Serializable{
     
     private static final long serialVersionUID = 1L;
@@ -36,10 +40,6 @@ public class ProductDTO implements Serializable{
     @NotNull(message = "Status do produto é obrigatório")
     private ProductStatus status;
 
-    public ProductDTO() {
-
-    }
-
     public ProductDTO(Product obj) {
         this.id = obj.getId();
         this.name = obj.getName();
@@ -49,73 +49,5 @@ public class ProductDTO implements Serializable{
         this.brand = obj.getBrand();
         this.category = obj.getCategory();
         this.status = obj.getStatus();
-    }
-
-    public static long getSerialversionuid() {
-        return serialVersionUID;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public BigDecimal getPrice() {
-        return price;
-    }
-
-    public void setPrice(BigDecimal price) {
-        this.price = price;
-    }
-
-    public int getQuantity() {
-        return quantity;
-    }
-
-    public void setQuantity(int quantity) {
-        this.quantity = quantity;
-    }
-
-    public String getCategory() {
-        return category;
-    }
-
-    public void setCategory(String category) {
-        this.category = category;
-    }
-
-    public String getBrand() {
-        return brand;
-    }
-
-    public void setBrand(String brand) {
-        this.brand = brand;
-    }
-
-    public ProductStatus getStatus() {
-        return status;
-    }
-
-    public void setStatus(ProductStatus status) {
-        this.status = status;
     }
 }
