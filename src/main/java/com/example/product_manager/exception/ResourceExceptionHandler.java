7bee58e801d2ecf4e9b8d19ca52f0bad275dfc0b
Adriano Borges Silva <120127965+AdrianoBorgesSilva@users.noreply.github.com>
2025-02-26 09:55:00 -0300

    Tratamento de exceção @Valid

--- src/main/java/com/example/product_manager/exception/ResourceExceptionHandler.java
+++ src/main/java/com/example/product_manager/exception/ResourceExceptionHandler.java
@@ -2,6 +2,7 @@ package com.example.product_manager.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -14,8 +15,18 @@ public class ResourceExceptionHandler {
     public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request) {
 
         HttpStatus status = HttpStatus.NOT_FOUND;
-        StandardError err = new StandardError(System.currentTimeMillis(), status.value(), "Not Found", e.getMessage(), request.getRequestURI());
+        StandardError err = new StandardError(System.currentTimeMillis(), status.value(), "Não Encontrado", e.getMessage(), request.getRequestURI());
         
+        return ResponseEntity.status(status).body(err);
+    }
+
+   @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<StandardError> validationError(MethodArgumentNotValidException e, HttpServletRequest request) {
+        
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        String mensagemErro = e.getBindingResult().getAllErrors().get(0).getDefaultMessage();
+
+        StandardError err = new StandardError(System.currentTimeMillis(), status.value(), "Erro de Validação do Corpo", mensagemErro, request.getRequestURI());
         return ResponseEntity.status(status).body(err);
     }
 }
